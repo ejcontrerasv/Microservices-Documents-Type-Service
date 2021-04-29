24dbdb54b47fe1c14bbb2e689a3ab2a5416c8187
ejcontrerasv <ejcontrerasv@gmail.com>
2021-04-29 00:10:13 -0400

    Correcciones

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO `documents_type` ( name, description, status_name) VALUES ('Anexo de Contrato','Anexos de Contratos ','Creado');
+INSERT INTO `documents_type` ( name, description, status_name) VALUES ('Liquidacion de Sueldo','Liquidaciones a sociadas a los trabajadores','Modificado');
+
